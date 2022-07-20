e22a1fb25b763ec5e041a81d8256f57ef6953a4b
Visanu <mvisanu@gmail.com>
2022-07-20 16:33:14 -0500

    add file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from router import blog_get, blog_post, user, article, product
+from router import blog_get, blog_post, user, article, product, file
 from auth import authentication
 from db import models
 from db.database import engine
@@ -9,6 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(authentication.router)
+app.include_router(file.router)
 app.include_router(user.router)
 app.include_router(article.router)
 app.include_router(product.router)
